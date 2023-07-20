d8794951ea6c62d21b80571a335f66b7e3183af6
dmhd6219 <svyatoslavsvyatkin@yandex.ru>
2023-07-20 12:53:31 +0300

    token response change

--- inno-fwd/main.py
+++ inno-fwd/main.py
@@ -109,4 +109,4 @@ async def login_for_access_token(email: str, password: str, db: Session = Depend
     access_token = auth.create_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return access_token
